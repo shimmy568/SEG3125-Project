6d2281af6503bdf8b4c6b85ed3d7dba79f0aee89
Maxim Melkonian <mmelk057@uottawa.ca>
2019-07-13 19:24:59 -0400

    BarGraph and RadarGraph components

---
+++ app/components/BarGraph/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BarChart from '../BarGraph/bar';
+
+export default function BGraph(){
+    return (
+        <div>
+            <BarChart></BarChart>
+        </div>
+    );
+}
---
+++ app/components/RadarChart/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RadarChart from './radar';
+
+export default function RChart(){
+    return(
+        <div>
+            <RadarChart/>
+        </div>
+    );
+}
