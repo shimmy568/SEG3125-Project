355cb2aebf612721d301f3c3d285058251bdd941
Owen Anderson <owenthomasanderson@gmail.com>
2019-07-14 12:44:25 -0400

    touched up some stuff

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -2,26 +2,30 @@ import React from 'react';
 import HelpButton from '../Help';
 import styled from 'styled-components';
 import UserProfile from '../UserProfile';
-import './style.css';
 
 function Footer() {
   const FooterDiv = styled.div`
     position: absolute;
     bottom: 0px;
+    left: 0px;
     width: 100%;
     height: 60px;
-    padding: 5px;
   `;
   const ReportAProblem = styled.a`
     position: absolute;
     bottom: 3px;
     right: 3px;
   `;
+  const ButtonDiv = styled.div`
+    position: absolute;
+    bottom: 3px;
+    left: 3px;
+  `;
   return (
     <FooterDiv>
-      <div className="float-left">
+      <ButtonDiv>
         <HelpButton/>
-      </div>
+      </ButtonDiv>
       {/* <UserProfile name="name" photo={require('../../images/logo.png')} description='description'/> */}
       <ReportAProblem href="#" className="float-right">Report a Problem</ReportAProblem>
     </FooterDiv>
