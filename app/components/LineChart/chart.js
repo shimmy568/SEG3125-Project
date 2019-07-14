054cb51737882c2ce54a661bcc9cc92c6861b696
Maxim Melkonian <mmelk057@uottawa.ca>
2019-07-14 16:02:52 -0400

    modified Line Graph

--- app/components/LineChart/chart.js
+++ app/components/LineChart/chart.js
@@ -12,18 +12,29 @@ const userData = [
     [18, 28, 20, 31, 36, 29, 9, 2, 2, 3, 10, 11]
 ];
 
+const singleViewData = [
+    [180, 5200, 1600, 2129, 1312, 120, 330, 239, 6320, 132, 924, 721],
+    [345, 2452, 2994, 2932, 329, 432, 2921, 1532, 2521, 2311, 2300, 1342],
+    [1329, 1420, 289, 192, 712, 4532, 3032, 134, 1221, 152, 196, 19],
+    [2344, 940, 2399, 2232, 2932, 392, 293, 302, 442, 472, 100, 2119],
+    [394, 593, 3953, 1204, 2943, 3902, 3592, 7004, 3942, 3942, 1392, 1149]
+];
+
 const userColors = ["#f56942", "#ffee6e", "#a6ff6e", "#8884d8", , "#82ca9d"]
 
 const months = ["Jan", "Feb", 'Mar', "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
-function populateData(numberOfUsers) {
+function populateData(numberOfUsers, names, isSingleView) {
     let finalData = [];
     for (var i = 0; i < 12; i++) {
         var tempObj = { "name": months[i] };
         for (var j = 0; j < numberOfUsers; j++) {
-            tempObj["user" + (j + 1)] = userData[j][i];
+            if (!isSingleView) {
+                tempObj[names[j]] = userData[j][i];
+            } else {
+                tempObj[names[j]] = singleViewData[j][i];
+            }
         }
-        console.log(tempObj);
         finalData.push(tempObj);
     }
     return finalData;
@@ -34,22 +45,32 @@ export default class Chart extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            numberOfLines: props.users
+            numberOfLines: props.users,
+            isSingleView: props.singleview,
+            allNames: props.usernames
         }
+        console.log(props);
     }
+
     render() {
         const lineElems = [];
+        let yAxisLabel = "";
         for (var i = 0; i < this.state.numberOfLines; i++) {
             lineElems.push(
-                <Line type="monotone" dataKey={"user" + (i + 1)} stroke={userColors[i]} />
+                <Line type="monotone" dataKey={this.state.allNames[i]} stroke={userColors[i]} key={i}/>
             );
+        }
+        if (this.state.isSingleView) {
+            yAxisLabel = "Lines Committed";
+        } else {
+            yAxisLabel = "Commits";
         }
         return (
             <ResponsiveContainer width="100%" height={300}>
                 <LineChart
                     width={500}
                     height={300}
-                    data={populateData(5)}
+                    data={populateData(this.state.numberOfLines, this.state.allNames, this.state.isSingleView)}
                     margin={{
                         top: 5, right: 30, left: 20, bottom: 5,
                     }}>
@@ -57,8 +78,8 @@ export default class Chart extends PureComponent {
                     <XAxis dataKey="name" height={40}>
                         <Label value="Month" offset={-1} position="insideBottom" />
                     </XAxis>
-                    <YAxis width={40} >
-                        <Label angle={270} value="Commits" position="left" style={{ textAnchor: 'middle' }}></Label>
+                    <YAxis width={60} >
+                        <Label angle={270} value={yAxisLabel} position="left" style={{ textAnchor: 'middle' }}></Label>
                     </YAxis>
                     <Tooltip />
                     <Legend verticalAlign="top" />
