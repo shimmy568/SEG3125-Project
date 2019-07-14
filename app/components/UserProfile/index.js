0ce1b460ed2c80a1485176d2f3a1e25c9d7d46ee
CalvinRossSmith <smithcalvin33@gmail.com>
2019-07-14 15:54:48 -0400

    adding

--- app/components/UserProfile/index.js
+++ app/components/UserProfile/index.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
+import { tsPropertySignature } from '@babel/types';
 import logo from '../../images/logo.png';
 import photo1 from '../../images/profilepic1.png';
 import photo2 from '../../images/profilepic2.png';
-import photo3 from "../../images/profilepic3.png";
-import photo4 from "../../images/profilepic4.png";
-import photo5 from "../../images/profilepic5.png";
-import { tsPropertySignature } from '@babel/types';
+import photo3 from '../../images/profilepic3.png';
+import photo4 from '../../images/profilepic4.png';
+import photo5 from '../../images/profilepic5.png';
 
 export default function UserProfile(props) {
-  let descriptions = ["I Like coding","I like photography","I live in Ottawa","I live in Toronto","I like birds"];
-  let photos = [photo1,photo2,photo3,photo4,photo5];
-  let colors = ['red','blue','pink','yellow','green']
-  let oppocolors = ['white','white','black','black','white']
-  let w =  parseInt(props.name[props.name.length - 1],10) % 5;
+  const descriptions = [
+    'I Like coding',
+    'I like photography',
+    'I live in Ottawa',
+    'I live in Toronto',
+    'I like birds',
+  ];
+  const photos = [photo1, photo2, photo3, photo4, photo5];
+  const colors = ['red', 'blue', 'pink', 'yellow', 'green'];
+  const oppocolors = ['white', 'white', 'black', 'black', 'white'];
+  const w = parseInt(props.name[props.name.length - 1], 10) % 5;
 
   let link = (
-    <Link to={`/single?user=${props.name}`} class="btn btn-primary" style={{backgroundColor: colors[w], borderColor: colors[w], color: oppocolors[w]}}>
+    <Link
+      to={`/single?user=${props.name}`}
+      class="btn btn-primary"
+      style={{
+        backgroundColor: colors[w],
+        borderColor: colors[w],
+        color: oppocolors[w],
+      }}
+    >
       View Profile
     </Link>
   );
-  if(props.isSingle){
+  if (props.isSingle) {
     link = (
-      <Link to={`https://github.com/${props.name}`} class="btn btn-primary" style={{backgroundColor: colors[w], borderColor: colors[w], color: oppocolors[w]}}>
+      <Link
+        to={`https://github.com/${props.name}`}
+        class="btn btn-primary"
+        style={{
+          backgroundColor: colors[w],
+          borderColor: colors[w],
+          color: oppocolors[w],
+        }}
+      >
         View Profile
       </Link>
     );
@@ -37,7 +59,7 @@ export default function UserProfile(props) {
         ) : (
           <img src={user} className="card-img-top" />
         )}
-        <div className="card-body" >
+        <div className="card-body">
           {props.name !== undefined ? (
             <h5 className="card-title">{props.name}</h5>
           ) : (
@@ -48,9 +70,7 @@ export default function UserProfile(props) {
           ) : (
             <p className="card-text">Unable to find description</p>
           )}
-          {props.name !== undefined ? (
-            link
-          ) : null}
+          {props.name !== undefined ? link : null}
         </div>
       </div>
     </div>
--- app/containers/MultiView/index.js
+++ app/containers/MultiView/index.js
@@ -5,11 +5,9 @@ import queryParamParse from 'utils/queryParamParse';
 import UserProfile from '../../components/UserProfile';
 import photo1 from '../../images/profilepic1.png';
 import photo2 from '../../images/profilepic2.png';
-import photo3 from "../../images/profilepic3.png";
-import photo4 from "../../images/profilepic4.png";
-import photo5 from "../../images/profilepic5.png";
-
-
+import photo3 from '../../images/profilepic3.png';
+import photo4 from '../../images/profilepic4.png';
+import photo5 from '../../images/profilepic5.png';
 
 class MultiView extends React.Component {
   constructor() {
@@ -18,25 +16,32 @@ class MultiView extends React.Component {
   }
 
   render() {
+    const usernames = queryParamParse(this.props.location.search);
 
-    let usernames = queryParamParse(this.props.location.search);
-    
-    if(usernames.length == 0){
-      return <Redirect to="/"/>
+    if (usernames.length == 0) {
+      return <Redirect to="/" />;
     }
-    let descriptions = ["I Like coding","I like photography","I live in Ottawa","I live in Toronto","I like birds"];
-    let photos = [photo1,photo2,photo3,photo4,photo5];
+    const descriptions = [
+      'I Like coding',
+      'I like photography',
+      'I live in Ottawa',
+      'I live in Toronto',
+      'I like birds',
+    ];
+    const photos = [photo1, photo2, photo3, photo4, photo5];
 
     return (
       <div>
         <Header />
         <p>This is the multi view page</p>
-        {usernames.map((user,index) => 
-          <UserProfile name={user} photo={photos[index]} description={descriptions[index]}/>
-        )}
-        
+        {usernames.map((user, index) => (
+          <UserProfile
+            name={user}
+            photo={photos[index]}
+            description={descriptions[index]}
+          />
+        ))}
       </div>
-      
     );
   }
 }
